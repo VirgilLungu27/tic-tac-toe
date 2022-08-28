a20a852f1a8d1517708b2d5e68b547422ce5ea46
Virgil <lunguvirgil3000@gmail.com>
2022-08-28 19:26:54 +0300

    Make the game fill the board at any time the test game button is pressed, without having to refresh the page

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const gameboard = document.querySelector('gameboard-container')
 const board = document.getElementsByClassName('cell')
 const choice = ["X", "O"]
 const cellList = []
-const button = document.getElementById('play')
+const button = document.getElementById('test')
+
+const play = document.getElementById('play')
+
 let boardCounter = 0
 
 const gameboardObject = {
@@ -23,10 +26,19 @@ button.addEventListener('click', screenController = () => {
     let index = Math.floor(Math.random() * choice.length);
     cell = choice[index];
     cellList.push(cell);
-  }
-  while (boardCounter < cellList.length) {
     board[boardCounter].innerHTML = `${cellList[boardCounter]}`;
     boardCounter += 1;
   } 
+  cellList.length = 0
+  boardCounter = 0
 })
 
+play.addEventListener('click', playerGame = () => {
+  for (cell in gameboardObject.gameboard) {
+    let index = Math.floor(Math.random() * choice.length);
+    cell = choice[index];
+    cellList.push(cell);
+    board[boardCounter].innerHTML = `${playerX.player}`;
+    boardCounter += 1;
+  }
+})
