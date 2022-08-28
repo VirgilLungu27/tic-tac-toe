9e028ca8eecd37c8b12291f759b225444909b8d8
Virgil <lunguvirgil3000@gmail.com>
2022-08-28 19:07:09 +0300

    add button and style it

--- script.js
+++ script.js
@@ -8,22 +8,30 @@ const gameboardObject = {
     gameboard: ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
 };
 
-const player = () => {
-    return "X";    
-};
-
-const opponent = () => {
-    return "O";
-}
-
 const screenController = () => {
   for (cell in gameboardObject.gameboard) {
     let index = Math.floor(Math.random() * choice.length);
     cell = choice[index];
     cellList.push(cell);
   }
-  while (boardCounter < cellList.length) {
+/*  while (boardCounter < cellList.length) {
     board[boardCounter].innerHTML = `${cellList[boardCounter]}`;
     boardCounter += 1;
-  }
+  } */
+}
+
+const playerGame = () => {
+    while (boardCounter < cellList.length) {
+        if (board[boardCounter].innerHTML == "") {
+           board[boardCounter].innerHTML = "X";
+           boardCounter += 1;
+        }
+        else {
+           console.log("Already occupied.")
+        }
+    }
+};
+
+const opponentGame = () => {
+    return "O";
 }
