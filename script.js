a339c771af750214fdfb8392ec280c71c12cd0c2
Virgil <lunguvirgil3000@gmail.com>
2022-08-28 10:36:03 +0300

    Define screen controller function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const gameboard = document.querySelector('gameboard-container')
 const board = document.getElementsByClassName('cell')
+const choice = ["X", "O"]
+const cellList = []
+let boardCounter = 0
 
 const gameboardObject = {
     gameboard: ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
@@ -13,3 +16,14 @@ const opponent = () => {
     return "O";
 }
 
+const screenController = () => {
+  for (cell in gameboardObject.gameboard) {
+    let index = Math.floor(Math.random() * choice.length);
+    cell = choice[index];
+    cellList.push(cell);
+  }
+  while (boardCounter < cellList.length) {
+    board[boardCounter].innerHTML = `${cellList[boardCounter]}`;
+    boardCounter += 1;
+  }
+}
