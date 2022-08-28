faeca1b5d254b7c0e7d756466b09f5e984d225ca
Virgil <lunguvirgil3000@gmail.com>
2022-08-28 19:10:14 +0300

    tie button to original screen controller

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const gameboard = document.querySelector('gameboard-container')
 const board = document.getElementsByClassName('cell')
 const choice = ["X", "O"]
 const cellList = []
+const button = document.getElementById('play')
 let boardCounter = 0
 
 const gameboardObject = {
     gameboard: ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
 };
 
-const screenController = () => {
+button.addEventListener('click', screenController = () => {
   for (cell in gameboardObject.gameboard) {
     let index = Math.floor(Math.random() * choice.length);
     cell = choice[index];
     cellList.push(cell);
   }
-/*  while (boardCounter < cellList.length) {
+  while (boardCounter < cellList.length) {
     board[boardCounter].innerHTML = `${cellList[boardCounter]}`;
     boardCounter += 1;
-  } */
-}
+  } 
+})
 
 const playerGame = () => {
     while (boardCounter < cellList.length) {
