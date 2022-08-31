032a728a1b8a1aca4cd8c10a7133b752b91c9a4b
Virgil <lunguvirgil3000@gmail.com>
2022-08-31 10:35:54 +0300

    Make movement stop whenever clicked length surpasses 5

--- script.js
+++ script.js
@@ -31,41 +31,42 @@ const boardGame = () => {
     choiceXY.push(x,y)
     clicked.push(choiceXY)
     console.log(clicked)
-    if (clicked[clicked.length - 1][1] < 58) {
-      if (clicked[clicked.length -1][0] < 548) {
-        board[0].innerHTML = `${playerX.player}`
-        }
-      else if(clicked[clicked.length -1][0] >= 548 && clicked[clicked.length-1][0] < 598) {
-        board[1].innerHTML = `${playerX.player}`      
-        }
-      else {
-        board[2].innerHTML = `${playerX.player}`
-      }
-      }
-    else if (clicked[clicked.length - 1][1] > 58 && clicked[clicked.length-1][1] < 108) {
+    if (clicked.length <= 5) {
+      if (clicked[clicked.length - 1][1] < 58) {
         if (clicked[clicked.length -1][0] < 548) {
-          board[3].innerHTML = `${playerX.player}`
+          board[0].innerHTML = `${playerX.player}`
           }
         else if(clicked[clicked.length -1][0] >= 548 && clicked[clicked.length-1][0] < 598) {
-          board[4].innerHTML = `${playerX.player}`      
+          board[1].innerHTML = `${playerX.player}`      
           }
         else {
-          board[5].innerHTML = `${playerX.player}`
+          board[2].innerHTML = `${playerX.player}`
+          }
         }
-        }
-    else {
-          if (clicked[clicked.length -1][0] < 548) {
-            board[6].innerHTML = `${playerX.player}`
-            }
-          else if(clicked[clicked.length -1][0] >= 548 && clicked[clicked.length-1][0] < 598) {
-            board[7].innerHTML = `${playerX.player}`      
+      else if (clicked[clicked.length - 1][1] > 58 && clicked[clicked.length-1][1] < 108) {
+        if (clicked[clicked.length -1][0] < 548) {
+            board[3].innerHTML = `${playerX.player}`
+          }
+        else if(clicked[clicked.length -1][0] >= 548 && clicked[clicked.length-1][0] < 598) {
+            board[4].innerHTML = `${playerX.player}`      
             }
           else {
-            board[8].innerHTML = `${playerX.player}`
+            board[5].innerHTML = `${playerX.player}`
+          }
+          }
+      else {
+            if (clicked[clicked.length -1][0] < 548) {
+              board[6].innerHTML = `${playerX.player}`
+              }
+            else if(clicked[clicked.length -1][0] >= 548 && clicked[clicked.length-1][0] < 598) {
+              board[7].innerHTML = `${playerX.player}`      
+              }
+            else {
+              board[8].innerHTML = `${playerX.player}`
+            }
           }
         }
     })
   }
   
-
-boardGame()
+  boardGame()
