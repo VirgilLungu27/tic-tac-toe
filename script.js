02ac63a0fbeaa8086217091ef331f142c4f2a8b8
Virgil <lunguvirgil3000@gmail.com>
2022-09-01 19:48:54 +0300

    add two input for player names

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const board = document.getElementsByClassName('cell')
 const button = document.getElementById('test')
 
 const play = document.getElementById('play')
-clicked = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 const playerX = {
   player: "X"
@@ -17,17 +16,18 @@ const playerO = {
 
 
 const boardGame = () => {
+  clicked = [0, 1, 2, 3, 4, 5, 6, 7, 8]
   container.addEventListener('click', screenXY = (e) => {
     for (turn in clicked) {  
         e.target = board[turn]
         if (turn % 2 == 0) {
-            e.target.innerHTML = `${playerX.player}`;
-          }
+          e.target.innerHTML = `${playerX.player}`;
+        }
         else if (turn % 2 == 1) {
-            e.target.innerHTML = `${playerO.player}`;
-          }
-     }
-    clicked.pop()
+          e.target.innerHTML = `${playerO.player}`;
+        }
+      }
+      clicked.pop()
   })
 } 
   
