5e0b1c903c9ea8057db5cd55dca986cbfab1cdaf
Virgil <lunguvirgil3000@gmail.com>
2022-09-04 10:48:29 +0300

    Define endGame function within the event listener. For now, works horizontally.

--- script.js
+++ script.js
@@ -16,6 +16,17 @@ const playerO = {
   player: "O"
 }
 
+const endGame = () => {
+  if (finalArray.length == 9) {
+    for (let i = 0; i < finalArray.length; i++) {
+      if ((finalArray[i] == finalArray[i+1]) && (finalArray[i+1] == finalArray[i+2]) && (finalArray[i] !== "") && ((i+3) % 3 == 0)) {
+        alert("Game over.")
+        window.location.href = "./index.html"
+      }
+    }
+  }
+}
+
 play.addEventListener('click', boardGame = () => {
   clicked = [0, 1, 2, 3, 4, 5, 6, 7, 8]
   const left = document.createElement("div")
@@ -41,20 +52,13 @@ play.addEventListener('click', boardGame = () => {
         for (cell in boardArray) {
           finalArray.push((boardArray[cell].textContent))
         }
+      endGame()
       })
       form.removeChild(play);
     })
+  
 
-
-function endGame() {
-  if ((finalArray[0] == boardArray[1]) && (boardArray[0] == boardArray[2]) && (boardArray[0] !== "")) {
-    alert("Game over.")
-    window.location.href = "./index.html"
-  }
-}
 
 reset.addEventListener('click', resetGame = () => {
   window.location.href = "./index.html"
 })
-
-endGame()
