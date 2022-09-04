47401d698c7f189bd4fa4419cdf8a6a70dc36a3c
Virgil <lunguvirgil3000@gmail.com>
2022-09-04 11:01:15 +0300

    Now works diagonally with clicked length lowered to 4 or lower

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const form = document.getElementById('form')
 
 let boardArray = Array.from(document.querySelectorAll('.cell'));
 let finalArray = []
+clicked = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
 
 const playerX = {
   player: "X"
@@ -20,17 +22,16 @@ const endGame = () => {
   if (finalArray.length == 9) {
     for (let i = 0; i < finalArray.length; i++) {
       if (finalArray[i] !== "") {
-        if (((finalArray[i] == finalArray[i+1]) && (finalArray[i+1] == finalArray[i+2]) && ((i+3) % 3 == 0)) || ((finalArray[i] == finalArray[i+3]) && (finalArray[i+3] == finalArray[i+6]))) {
+        if (((finalArray[i] == finalArray[i+1]) && (finalArray[i+1] == finalArray[i+2]) && ((i+3) % 3 == 0)) || ((finalArray[i] == finalArray[i+3]) && (finalArray[i+3] == finalArray[i+6])) || (((finalArray[0] == finalArray[4]) && (finalArray[4] == finalArray[8])) || ((finalArray[2] == finalArray[4]) && (finalArray[4] == finalArray[6]))) && (clicked.length <= 4)) {
           alert("Game over.")
           window.location.href = "./index.html"
-        }
+        } 
       }
     }
   }
 }
 
 play.addEventListener('click', boardGame = () => {
-  clicked = [0, 1, 2, 3, 4, 5, 6, 7, 8]
   const left = document.createElement("div")
   const right = document.createElement("div")
   const leftPlayer = document.getElementById("left")
