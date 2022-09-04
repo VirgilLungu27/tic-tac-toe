8b947a190a91ba44a8e833738a7e6576fdd40c9e
Virgil <lunguvirgil3000@gmail.com>
2022-09-04 11:13:28 +0300

    Alert the winner and congratulate them

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const play = document.getElementById('play')
 const reset = document.getElementById('reset')
 const form = document.getElementById('form')
 
+const leftPlayer = document.getElementById("left")
+const rightPlayer = document.getElementById("right")
+
 let boardArray = Array.from(document.querySelectorAll('.cell'));
 let finalArray = []
 clicked = [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -23,23 +26,20 @@ const endGame = () => {
     for (let i = 0; i < finalArray.length; i++) {
       if (finalArray[i] !== "") {
         if (((finalArray[i] == finalArray[i+1]) && (finalArray[i+1] == finalArray[i+2]) && ((i+3) % 3 == 0)) || ((finalArray[i] == finalArray[i+3]) && (finalArray[i+3] == finalArray[i+6])) || (((finalArray[0] == finalArray[4]) && (finalArray[4] == finalArray[8])) || ((finalArray[2] == finalArray[4]) && (finalArray[4] == finalArray[6]))) && (clicked.length <= 4)) {
-          alert("Game over.")
-          window.location.href = "./index.html"
-        } 
+          if (finalArray[i] == "X") {
+            alert("Game over. Congratulations " + leftPlayer.value + "!")
+         }
+          else if (finalArray[i] == "O") {
+            alert("Game over. Congratulations " + rightPlayer.value + "!")
+         }
+         window.location.href = "./index.html" 
+        }
       }
     }
   }
 }
 
 play.addEventListener('click', boardGame = () => {
-  const left = document.createElement("div")
-  const right = document.createElement("div")
-  const leftPlayer = document.getElementById("left")
-  const rightPlayer = document.getElementById("right")
-  left.innerHTML = `${leftPlayer.value}`
-  right.innerHTML = `${rightPlayer.value}`
-  screen.appendChild(left);
-  screen.appendChild(right)
   container.addEventListener('click', screenXY = (e) => {
       finalArray = []
       for (turn in clicked) {  
